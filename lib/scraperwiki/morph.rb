fe56388593fb22699ee2858e69e9a1081eba4fc8
Matthew Landauer <matthew@openaustralia.org>
2014-01-14 15:21:02 +1100

    Make skeleton gem with bundle gem scraperwiki-morph

---
+++ lib/scraperwiki/morph.rb
@@ -0,0 +1,7 @@
+require "scraperwiki/morph/version"
+
+module Scraperwiki
+  module Morph
+    # Your code goes here...
+  end
+end
---
+++ lib/scraperwiki/morph/version.rb
@@ -0,0 +1,5 @@
+module Scraperwiki
+  module Morph
+    VERSION = "0.0.1"
+  end
+end
