fe83aa6f7e96895eefae78b103abf3ad2fcb72af
Matthew Landauer <matthew@openaustralia.org>
2014-01-14 16:04:36 +1100

    Add verbose parameter

--- lib/scraperwiki-morph.rb
+++ lib/scraperwiki-morph.rb
@@ -2,7 +2,7 @@ require "scraperwiki-morph/version"
 require "scraperwiki"
 
 module ScraperWikiMorph
-  def self.save_sqlite(unique_keys, data, table = "data")
-    ScraperWiki.save_sqlite(unique_keys, data, table)
+  def self.save_sqlite(unique_keys, data, table = "data", verbose = 0)
+    ScraperWiki.save_sqlite(unique_keys, data, table, verbose)
   end
 end
--- spec/scraperwiki-morph_spec.rb
+++ spec/scraperwiki-morph_spec.rb
@@ -4,14 +4,20 @@ describe ScraperWikiMorph do
   describe ".save_sqlite" do
     it "should use the data table by default" do
       unique_keys, data = double, double
-      ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, "data")
+      ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, "data", 0)
       ScraperWikiMorph.save_sqlite(unique_keys, data)
     end
 
     it "should use whatever table you want" do
       unique_keys, data, table = double, double, double
-      ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, table)
+      ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, table, 0)
       ScraperWikiMorph.save_sqlite(unique_keys, data, table)
+    end
+
+    it "should support the verbose parameter" do
+      unique_keys, data, table, verbose = double, double, double, double
+      ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, table, verbose)
+      ScraperWikiMorph.save_sqlite(unique_keys, data, table, verbose)
     end
   end
 end
