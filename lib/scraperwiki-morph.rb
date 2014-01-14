5bbf546585f2a4a4407f40ac8bd0d65b9486f885
Matthew Landauer <matthew@openaustralia.org>
2014-01-14 16:10:31 +1100

    Set the database name before every call

--- lib/scraperwiki-morph.rb
+++ lib/scraperwiki-morph.rb
@@ -3,6 +3,7 @@ require "scraperwiki"
 
 module ScraperWikiMorph
   def self.save_sqlite(unique_keys, data, table = "data", verbose = 0)
+    ScraperWiki.config={db: 'data.sqlite'}
     ScraperWiki.save_sqlite(unique_keys, data, table, verbose)
   end
 end
--- spec/scraperwiki-morph_spec.rb
+++ spec/scraperwiki-morph_spec.rb
@@ -4,18 +4,21 @@ describe ScraperWikiMorph do
   describe ".save_sqlite" do
     it "should use the data table by default" do
       unique_keys, data = double, double
+      ScraperWiki.should_receive(:config=).with({db: "data.sqlite"})
       ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, "data", 0)
       ScraperWikiMorph.save_sqlite(unique_keys, data)
     end
 
     it "should use whatever table you want" do
       unique_keys, data, table = double, double, double
+      ScraperWiki.should_receive(:config=).with({db: "data.sqlite"})
       ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, table, 0)
       ScraperWikiMorph.save_sqlite(unique_keys, data, table)
     end
 
     it "should support the verbose parameter" do
       unique_keys, data, table, verbose = double, double, double, double
+      ScraperWiki.should_receive(:config=).with({db: "data.sqlite"})
       ScraperWiki.should_receive(:save_sqlite).with(unique_keys, data, table, verbose)
       ScraperWikiMorph.save_sqlite(unique_keys, data, table, verbose)
     end
